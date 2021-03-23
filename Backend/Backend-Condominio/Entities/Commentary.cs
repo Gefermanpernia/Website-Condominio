35c3862d77098b2bade5a5117b5b91f71ec60356
Geferman <geferman.pro@gmail.com>
2021-03-23 01:38:32 -0400

    feat (entities): Creación de entidades closed (#8)

---
+++ Backend/Backend-Condominio/Entities/Commentary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Condominio.Entities
+{
+    public class Commentary
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Content { get; set; }
+
+        public int CoversationTopicId { get; set; }
+
+        public ConversationTopic ConversationTopic { get; set; }
+
+    }
+}
