fb366f9617372790482bbca5b6e705f883d6dfbf
cgerabit <cristian@faucetcollector.com>
2021-04-02 19:07:00 -0400

    (/backend) Servicio envio de emails (fix #54)

---
+++ Backend/Backend-Condominio/DTOs/Mail/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend_Condominio.DTOs.Mail
+{
+    public class MailRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Destination { get; set; }
+    }
+}
