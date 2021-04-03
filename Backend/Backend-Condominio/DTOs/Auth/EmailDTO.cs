b029812bd358c7757f7182ae2f447d012a988e3d
Geferman Pernia <33972248+Gefermanpernia@users.noreply.github.com>
2021-04-02 20:25:38 -0400

    feat (Backend/Backend-Condominio/Controllers/AccountsController.cs): creacion de reset password close #9

---
+++ Backend/Backend-Condominio/DTOs/Auth/EmailDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Condominio.DTOs.Auth
+{
+    public class EmailDTO
+    {
+        public string Email { get; set; }
+    }
+}
